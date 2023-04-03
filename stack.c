7776bb88bf0e2f5431495bbe396c96c23c96d311
Abhisehk Chauhan <abhishekprogrammer18112003@gmail.com>
2023-04-03 14:18:00 +0530

    demo

--- stack.c
+++ stack.c
@@ -46,7 +46,7 @@
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <stddef.h>
